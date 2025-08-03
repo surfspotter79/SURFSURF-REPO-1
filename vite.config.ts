db6c1f31b52f9f95f2b69298df615ef52d9da81a
surfspotter79 <matthieu.barthlen@gmail.com>
2025-08-03 12:29:19 +0200

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
+  base: '/SURFSURF-REPO-1/', // Nom EXACT de ton dépôt GitHub (sensible à la casse)
   plugins: [react()],
-  base: './', // ✅ Nécessaire pour que les chemins fonctionnent sur GitHub Pages
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: path.resolve(__dirname, 'index.html'),
+    },
+  },
 });
